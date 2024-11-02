a8d7f7bc4442d262615304ffc5e0d4e7f8d8361e
oladotun <oladotunadekomaya@gmail.com>
2024-11-02 21:32:36 +0100

    commit

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ import http from 'http';
 const PORT = 8000;
 
 const server = http.createServer((req,res) => {
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<h1>Hello World</h1>');
-    res.statusCode = 404;
-    res.statusMessage = "Fuck You";
+    // res.setHeader('Content-Type', 'text/html')
+
     // res.write('<h1>Hello World</h1>');
-    res.end();
+    // res.statusCode = 404;    // res.write('<h1>Hello World</h1>');
+    // res.statusCode = 404;
+    // res.statusMessage = "Fuck You";
+    // res.statusMessage = "Fuck You";
+
+    res.writeHead(500, "fuckkkk!!!", {'Content-Type': 'application/json'})
+
+
+    res.end(JSON.stringify({message: 'Server Error'}));
 })
 
 server.listen(PORT, ()=> {
