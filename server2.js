d76c9471cea559af0eb3e6b03a6774c4baa61be2
oladotun <oladotunadekomaya@gmail.com>
2024-11-03 20:00:35 +0100

    commit

--- server2.js
+++ server2.js
@@ -1,7 +1,7 @@
 import { createServer } from 'http';
 const PORT = process.env.PORT;
 
-const user = [
+const users = [
     {id: 1, name: 'John Doe'},
     {id: 2, name: 'Jane Doe'},
     {id: 3, name: 'Fuck Doe'},
@@ -10,12 +10,25 @@ const user = [
 const server = createServer((req,res) =>{
     if (req.url === '/api/users' && req.method === 'GET') {
         res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(user))
+        res.write(JSON.stringify(users))
         res.end()
-    } else {
+    } else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET"){
+        res.setHeader('Content-Type', 'application/json');
+        const id = req.url.split('/')[3];
+        const user = users.find(user => user.id === parseInt(id));
+        if (user) {
+            res.write(JSON.stringify(user))
+            res.end()
+        } else {
+            
+            res.statusCode = 404;
+            res.write(JSON.stringify({message : "User not found"}))
+            res.end()
+        }
+    }else {
         res.setHeader('Content-Type', 'application/json');
         res.statusCode = 404;
-        res.write(JSON.stringify({message : "route not found"}))
+        res.write(JSON.stringify({message : "Route not found"}))
         res.end()
     }
 
