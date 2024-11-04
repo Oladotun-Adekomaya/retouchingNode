60b5adb06127f17599483fb92fbe88724982133d
oladotun <oladotunadekomaya@gmail.com>
2024-11-04 14:29:15 +0100

    commit

--- asynchronousjs/promises.js
+++ asynchronousjs/promises.js
@@ -26,15 +26,38 @@ function createPost (post) {
             } else {
                 reject('Error: Something went wrong')
             }
-
-
         }, 2000);
     })
     
 }
 
 
-createPost({ title: 'Post Three', body: 'This is post three'})
-    .then(getPosts)
-    .catch((err) => console.log(err))
+// createPost({ title: 'Post Three', body: 'This is post three'})
+//     .then(getPosts)
+//     .catch((err) => console.log(err))
 
+
+// Async / Await 
+
+async function init() {
+   await createPost({ title: 'Post Three', body: 'This is post three' })
+   getPosts()
+}
+
+init()
+
+
+
+
+// Promise.all
+
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10
+// const promise3 = new Promise((resolve,reject) =>
+//     setTimeout(resolve, 2000, 'Goodbye'))
+
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+// .then(response => response.json())
+
+// Promise.all([promise1, promise2, promise3,promise4]).then(values => console.log(values)
+// );
